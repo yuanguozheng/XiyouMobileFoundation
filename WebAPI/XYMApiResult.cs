c8961e9ac449fcee0f9e98de6dd6accc44c9ba42
yuanguozheng <yuanguozheng@outlook.com>
2015-05-05 02:05:13 +0800

    Init Repo, Add Files

---
+++ WebAPI/XYMApiResult.cs
@@ -0,0 +1,94 @@
+﻿/**
+ * Copyright (C) 2015 Xiyou Mobile Application Lab.
+ * 
+ * XiyouMobileMVCLib
+ * 
+ * XYMApiResult.cs
+ * 
+ * Created by Yuan Guozheng on 2015-5-5.
+ */
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XiyouMobileMVCLib.WebAPI
+{
+    /// <summary>
+    /// 返回Api结果
+    /// </summary>
+    public class XYMApiResult : JsonResult
+    {
+        /// <summary>
+        /// 默认Content-Type
+        /// </summary>
+        private const string DEFAULT_CONTENT_TYPE = "application/json; charset=utf-8";
+
+        /// <summary>
+        /// JSONP回调方法名参数名
+        /// </summary>
+        private static string JsonpParam = "callback";
+
+        /// <summary>
+        /// 保存数据
+        /// </summary>
+        private object data = null;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public XYMApiResult()
+        {
+            this.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+        }
+
+        /// <summary>
+        /// 带返回数据的构造函数
+        /// </summary>
+        /// <param name="data">要返回的数据</param>
+        public XYMApiResult(object data)
+        {
+            this.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            this.data = data;
+        }
+
+        /// <summary>
+        /// 设置JSONP回调函数名参数名
+        /// </summary>
+        /// <param name="param">参数名</param>
+        public void setJsonpParam(string param)
+        {
+            if (string.IsNullOrWhiteSpace(param))
+            {
+                return;
+            }
+            JsonpParam = param;
+        }
+
+        /// <summary>
+        /// 执行
+        /// </summary>
+        /// <param name="controllerContext">控制器上下文</param>
+        public override void ExecuteResult(ControllerContext controllerContext)
+        {
+            if (controllerContext != null)
+            {
+                HttpResponseBase Response = controllerContext.HttpContext.Response;
+                HttpRequestBase Request = controllerContext.HttpContext.Request;
+                string callbackfunction = Request[JsonpParam];
+                Response.ContentType = DEFAULT_CONTENT_TYPE;
+                if (data != null)
+                {
+                    if (string.IsNullOrWhiteSpace(callbackfunction))
+                        Response.Write(JsonConvert.SerializeObject(data));
+                    else
+                        Response.Write(string.Format("{0}({1})", callbackfunction, JsonConvert.SerializeObject(data)));
+                }
+            }
+        }
+    }
+}
---
+++ WebAPI/XYMUniResult.cs
@@ -0,0 +1,33 @@
+﻿/**
+ * Copyright (C) 2015 Xiyou Mobile Application Lab.
+ * 
+ * XiyouMobileMVCLib
+ * 
+ * XYMUniResult.cs
+ * 
+ * Created by Yuan Guozheng on 2015-5-5.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XiyouMobileMVCLib.WebAPI
+{
+    /// <summary>
+    /// 通用返回结果
+    /// </summary>
+    public class XYMUniResult
+    {
+        /// <summary>
+        /// 标识是否成功
+        /// </summary>
+        public bool Result { get; set; }
+        
+        /// <summary>
+        /// 返回详情
+        /// </summary>
+        public object Detail { get; set; }
+    }
+}
